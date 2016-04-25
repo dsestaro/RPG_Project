ed159b19195c6d2dc14e04c2926937e769651cf2
Davidson Sestaro <dsestaro@gmail.com>
2016-04-25 12:43:46 -0300

    Creation of character alignments enum and their test

---
+++ rpg-project-core/src/main/java/br/com/sestaro/characters/commons/info/enums/CharacterAlignNames.java
@@ -0,0 +1,23 @@
+package br.com.sestaro.characters.commons.info.enums;
+
+public enum CharacterAlignNames {
+	LAWFUL_GOOD(1),
+	NEUTRAL_GOOD(2),
+	CHAOTIC_GOOD(3),
+	LAWFUL_NEUTRAL(4),
+	NEUTRAL(5),
+	CHAOTIC_NEUTRAL(6),
+	LAWFUL_EVIL(7),
+	NEUTRAL_EVIL(8),
+	CHAOTIC_EVIL(9);
+	
+	private final int position;
+	
+	CharacterAlignNames(int position) {
+		this.position = position;
+	}
+	
+	public int getPosition() {
+		return this.position;
+	}
+}
---
+++ rpg-project-core/src/test/java/br/com/sestaro/character/commons/info/enums/CharacterAlignNamesTest.java
@@ -0,0 +1,23 @@
+package br.com.sestaro.character.commons.info.enums;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import br.com.sestaro.characters.commons.info.enums.CharacterAlignNames;
+
+public class CharacterAlignNamesTest {
+	
+	@Test
+	public void enumTest() {
+		assertEquals(1, CharacterAlignNames.LAWFUL_GOOD.getPosition());
+		assertEquals(2, CharacterAlignNames.NEUTRAL_GOOD.getPosition());
+		assertEquals(3, CharacterAlignNames.CHAOTIC_GOOD.getPosition());
+		assertEquals(4, CharacterAlignNames.LAWFUL_NEUTRAL.getPosition());
+		assertEquals(5, CharacterAlignNames.NEUTRAL.getPosition());
+		assertEquals(6, CharacterAlignNames.CHAOTIC_NEUTRAL.getPosition());
+		assertEquals(7, CharacterAlignNames.LAWFUL_EVIL.getPosition());
+		assertEquals(8, CharacterAlignNames.NEUTRAL_EVIL.getPosition());
+		assertEquals(9, CharacterAlignNames.CHAOTIC_EVIL.getPosition());
+	}
+}
