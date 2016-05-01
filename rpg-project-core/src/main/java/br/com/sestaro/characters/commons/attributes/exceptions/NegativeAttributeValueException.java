10ad8e2a7169c9618c3dee4d76cae35d284e6629
Davidson Sestaro <dsestaro@gmail.com>
2016-05-01 13:52:46 -0300

    Checkstyle code adapt

--- rpg-project-core/src/main/java/br/com/sestaro/characters/commons/attributes/exceptions/NegativeAttributeValueException.java
+++ rpg-project-core/src/main/java/br/com/sestaro/characters/commons/attributes/exceptions/NegativeAttributeValueException.java
@@ -1,8 +1,16 @@
 package br.com.sestaro.characters.commons.attributes.exceptions;
 
+/**
+ * Exception to handle negative value to attributes.
+ * 
+ * @author davidson.sestaro
+ */
 public class NegativeAttributeValueException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Default constructor.
+	 */
 	public NegativeAttributeValueException() {
 		super("The attribute value cannot be negative!");
 	}
--- rpg-project-core/src/main/java/br/com/sestaro/characters/commons/attributes/exceptions/NullAttributesException.java
+++ rpg-project-core/src/main/java/br/com/sestaro/characters/commons/attributes/exceptions/NullAttributesException.java
@@ -1,8 +1,16 @@
 package br.com.sestaro.characters.commons.attributes.exceptions;
 
+/**
+ * Exception to handle null value to attributes.
+ * 
+ * @author davidson.sestaro
+ */
 public class NullAttributesException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Default constructor.
+	 */
 	public NullAttributesException() {
 		super("The attribute variable cannot be null!");
 	}
