f5bb159bda5b46b5b5444a99bdcecce38294e73e
Davidson Sestaro <dsestaro@gmail.com>
2016-05-02 16:06:03 -0300

    Addiction of constant classes to reduce the size and the code duplicated

---
+++ rpg-project-core/src/test/java/br/com/sestaro/characters/commons/attributes/enums/constants/AttributeNamePositionConstantValues.java
@@ -0,0 +1,27 @@
+package br.com.sestaro.characters.commons.attributes.enums.constants;
+
+/**
+ * Class with constant values for the test classes.
+ * 
+ * @author davidson.sestaro
+ */
+public final class AttributeNamePositionConstantValues {
+  public static final int STRENGTH_POSITION = 10;
+  public static final int STRENGTH_PHYSICAL_STRENGTH_POSITION = 11;
+  public static final int STRENGTH_ENDURANCE_POSITION = 12;
+  public static final int DEXTERITY_POSITION = 20;
+  public static final int DEXTERITY_SPEED_POSITION = 21;
+  public static final int DEXTERITY_COORDINATION_POSITION = 22;
+  public static final int CONSTITUTION_POSITION = 30;
+  public static final int CONSTITUTION_TOUGHNESS_POSITION = 31;
+  public static final int CONSTITUTION_RESISTANCE_POSITION = 32;
+  public static final int INTELLIGENCE_POSITION = 4;
+  public static final int WISDOM_POSITION = 5;
+  public static final int CHARISMA_POSITION = 6;
+  
+  /**
+   * Constructor to prevent class instantiation.
+   */
+  private AttributeNamePositionConstantValues() {
+  }
+}
---
+++ rpg-project-core/src/test/java/br/com/sestaro/characters/commons/attributes/values/constants/NumericValuesConstantValues.java
@@ -0,0 +1,30 @@
+package br.com.sestaro.characters.commons.attributes.values.constants;
+
+/**
+ * Class with constant values for the test classes.
+ * 
+ * @author davidson.sestaro
+ */
+public final class NumericValuesConstantValues {
+  public static final int NEGATIVE_VALUE = -2;
+  public static final int POSITIVE_VALUE = 2;
+  
+  public static final int FIVE_VALUE = 5;
+  public static final int TWELVE_VALUE = 12;
+  public static final int THIRTEEN_VALUE = 13;
+  public static final int FOURTEEN_VALUE = 14;
+  public static final int FIFTEEN_VALUE = 15;
+  public static final int SIXTEEN_VALUE = 16;
+  public static final int SEVENTEEN_VALUE = 17;
+  public static final int EIGHTEEN_VALUE = 18;
+  public static final int NINETEEN_VALUE = 19;
+  
+  public static final int FIVE_MODIFIER = -2;
+  public static final int EIGHTEEN_MODIFIER = 4;
+  
+  /**
+   * Constructor to prevent class instantiation.
+   */
+  private NumericValuesConstantValues() {
+  }
+}
