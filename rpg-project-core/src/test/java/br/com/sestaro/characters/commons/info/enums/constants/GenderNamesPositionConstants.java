bd8db57c11d53c9a8089488efaa18c0a9f45b994
Davidson Sestaro <dsestaro@gmail.com>
2016-05-03 11:04:05 -0300

    Creation of constant classes to eliminate duplicated code

---
+++ rpg-project-core/src/test/java/br/com/sestaro/characters/commons/info/enums/constants/GenderNamesPositionConstants.java
@@ -0,0 +1,17 @@
+package br.com.sestaro.characters.commons.info.enums.constants;
+
+/**
+ * Class with constant values for the test classes.
+ * 
+ * @author davidson.sestaro
+ */
+public final class GenderNamesPositionConstants {
+  public static final int MAN = 1;
+  public static final int WOMAN = 2;
+  
+  /**
+   * Constructor to prevent class instantiation.
+   */
+  private GenderNamesPositionConstants() {
+  }
+}
