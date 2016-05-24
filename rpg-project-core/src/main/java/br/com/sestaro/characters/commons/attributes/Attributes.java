bfd12249a07598ed6de33a10aff71ac1a43076ea
Davidson Sestaro <dsestaro@gmail.com>
2016-05-24 08:13:17 -0300

    Correcting imports

--- rpg-project-core/src/main/java/br/com/sestaro/characters/commons/attributes/Attributes.java
+++ rpg-project-core/src/main/java/br/com/sestaro/characters/commons/attributes/Attributes.java
@@ -2,10 +2,6 @@ package br.com.sestaro.characters.commons.attributes;
 
 import javax.persistence.Entity;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-
 import br.com.sestaro.characters.commons.attributes.enums.AttributesNames;
 import br.com.sestaro.characters.commons.attributes.exceptions.NullAttributesException;
 import br.com.sestaro.characters.commons.attributes.generalization.AttributesAbstract;
