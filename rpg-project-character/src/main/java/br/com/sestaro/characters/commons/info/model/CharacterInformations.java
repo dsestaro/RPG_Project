6e14e39ccd4fb6953cb306b269c8274aa379848d
Davidson Sestaro <dsestaro@gmail.com>
2016-06-22 17:21:03 -0300

    Correcting class layout

--- rpg-project-character/src/main/java/br/com/sestaro/characters/commons/info/model/CharacterInformations.java
+++ rpg-project-character/src/main/java/br/com/sestaro/characters/commons/info/model/CharacterInformations.java
@@ -1,31 +1,25 @@
 package br.com.sestaro.characters.commons.info.model;
 
 import br.com.sestaro.characters.commons.info.model.exceptions.InvalidInformationException;
+import br.com.sestaro.characters.commons.info.model.generalization.CharacterInformationsAbstract;
 import br.com.sestaro.characters.commons.info.model.values.CharacterInformationsValues;
 
 /**
- * Class to interact and manipulate the character information. 
+ * Class to interact and manipulate the character information.
  * 
  * @author davidson.sestaro
  */
-public class CharacterInformations {
-	private CharacterInformationsValues values;
-
-	/**
-	 * @param value                                          - Character infomration values
-	 */
-	public CharacterInformations(final CharacterInformationsValues value) {
-		if (value == null) {
-			throw new InvalidInformationException();
-		}
-		
-		this.values = value;
+public class CharacterInformations extends CharacterInformationsAbstract {
+    
+    /**
+     * @param value
+     *            - Character information values
+     */
+    public CharacterInformations(final CharacterInformationsValues value) {
+	if (value == null) {
+	    throw new InvalidInformationException();
 	}
 
-	/**
-	 * @return                                                - Character information values
-	 */
-	public final CharacterInformationsValues getValues() {
-		return values;
-	}
+	setValues(value);
+    }
 }
