d5e3dfbb3bd37def16063514ee31cabb753cb1bb
Davidson Sestaro <dsestaro@gmail.com>
2016-06-22 17:20:28 -0300

    Change of how hibernate import the annotated classes

---
+++ rpg-project-character/src/main/java/br/com/sestaro/characters/commons/info/model/generalization/CharacterInformationsAbstract.java
@@ -0,0 +1,52 @@
+package br.com.sestaro.characters.commons.info.model.generalization;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.OneToOne;
+
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
+
+import br.com.sestaro.characters.commons.info.model.values.CharacterInformationsValues;
+
+/**
+ * Abstract class to interact and manipulate the attribute values. 
+ * 
+ * @author davidson.sestaro
+ */
+@MappedSuperclass
+public abstract class CharacterInformationsAbstract {
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  
+  @OneToOne(targetEntity = CharacterInformationsValues.class)
+  @Cascade(CascadeType.ALL)
+  private CharacterInformationsValues values;
+  
+  /**
+   * @return                                   - Id value
+   */
+  public final Long getId() {
+    return id;
+  }
+
+  /**
+   * @return                                   - Attribute values
+   */
+  public final CharacterInformationsValues getValues() {
+    return values;
+  }
+
+  /**
+   * Set the attributes values.
+   * 
+   * @param value                               - Attribute values
+   */
+  public final void setValues(final CharacterInformationsValues value) {
+    this.values = value;
+  }
+}
--- rpg-project-character/src/main/java/br/com/sestaro/characters/dao/factory/SessionFactory.java
+++ rpg-project-character/src/main/java/br/com/sestaro/characters/dao/factory/SessionFactory.java
@@ -1,31 +1,50 @@
 package br.com.sestaro.characters.dao.factory;
 
+import java.util.Set;
+
 import org.hibernate.Session;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.reflections.Reflections;
 
 /**
- * Class to create hibernate sessions in a factory way. 
+ * Class to create hibernate sessions in a factory way.
  * 
  * @author davidson.sestaro
  */
 public final class SessionFactory {
 
     private static org.hibernate.SessionFactory sf;
-    
+
     /**
      * Private constructor to prevent instantiation.
      */
     private SessionFactory() {
     }
-    
+
     /**
-     * @return					- Hibernate Session
+     * @return - Hibernate Session
      */
     public static Session getSession() {
 	if (sf == null) {
-	    sf = new Configuration().configure().buildSessionFactory();
+	    Configuration configuration = new Configuration();
+
+	    configuration.configure();
+
+	    Reflections reflections = new Reflections("br.com.sestaro.characters.commons");
+
+	    Set<Class<?>> classes = reflections.getTypesAnnotatedWith(javax.persistence.Entity.class);
+
+	    for (Class<?> clazz : classes) {
+		configuration.addAnnotatedClass(clazz);
+	    }
+
+	    ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+		    .applySettings(configuration.getProperties()).build();
+	    sf = configuration.buildSessionFactory(serviceRegistry);
 	}
-	
+
 	return sf.openSession();
     }
 }
